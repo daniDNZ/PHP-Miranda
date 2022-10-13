efa8d88675d746b1c6cd2ddf28cbae88455c3da2
danisanz <daniDNZ@users.noreply.github.com>
2022-10-13 23:02:41 +0200

    db connection

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+include './env.php';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
